ee63b539aae1c221e35e24b4fd7d93573f2a3a04
manxia.pc <manxia.pc@alibaba-inc.com>
2019-07-19 00:33:05 +0800

    feat: style

--- src/components/Introductor.vue
+++ src/components/Introductor.vue
@@ -54,6 +54,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 li{
-  list-style-type:none;
+  list-style-type: none;
+  text-align: left;
 }
 </style>
